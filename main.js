920dc9348447024cc37955d2a31d55a54da5bac9
FrostBaer <frostbaery@gmail.com>
2023-11-04 17:43:29 +0100

    Resize done.

---
+++ main.js
@@ -0,0 +1,72 @@
+import './style.css'
+import * as THREE from "three";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+//Variables for setup
+
+let container;
+let camera;
+let renderer;
+let scene;
+let house;
+
+function init() {
+  container = document.querySelector('.scene');
+
+  //Create scene
+  scene = new THREE.Scene();
+
+  //Camera
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 500;
+
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 5, 30);
+
+  //Lights
+  const ambient = new THREE.AmbientLight(0x404040, 10);
+
+  const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+  dirLight.position.set(10, 10, 0);
+  
+  scene.add(ambient, dirLight);
+
+  //Renderer
+  //élsimítás + átlátszó háttér
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load model
+  let loader = new GLTFLoader();
+  loader.load('./assets/elf-house/scene.gltf', function (gltf) {
+    scene.add(gltf.scene);
+    house = gltf.scene.children[0];
+    animate();
+  });
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  house.rotation.z += 0.005;
+  renderer.render(scene, camera);
+}
+
+
+init();
+
+
+//Responsive window
+
+function onWindowResize(){
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener('resize', onWindowResize);
